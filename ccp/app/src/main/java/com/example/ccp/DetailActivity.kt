ef794cbaee1f9b627a80a4e0450e50e7d4c845f5
HwangJanghyeon <janghyeonh@naver.com>
2024-03-26 14:03:59 +0900

    안드로이드 인서트액티비티, 업데이트액티비티 파일 및 레이아웃만 추가

--- ccp/app/src/main/java/com/example/ccp/DetailActivity.kt
+++ ccp/app/src/main/java/com/example/ccp/DetailActivity.kt
@@ -1,11 +1,36 @@
 package com.example.ccp
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ccp.databinding.ActivityDetailBinding
+
 
 class DetailActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityDetailBinding
+    private lateinit var detailTitleEditText: EditText
+    private lateinit var detailWriterEditText: EditText
+    private lateinit var detailContentEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        detailTitleEditText = findViewById(R.id.insertTitle)
+        detailWriterEditText = findViewById(R.id.insertlWriter)
+        detailContentEditText = findViewById(R.id.insertContent)
+
+        // Intent에서 DTO 객체의 정보를 받아옴
+        val boardTitle = intent.getStringExtra("board_title")
+        val boardWriter = intent.getStringExtra("board_writer")
+        val boardContent = intent.getStringExtra("board_content")
+
+        // 받아온 정보를 EditText에 설정
+        detailTitleEditText.setText(boardTitle)
+        detailWriterEditText.setText(boardWriter)
+        detailContentEditText.setText(boardContent)
     }
 }
