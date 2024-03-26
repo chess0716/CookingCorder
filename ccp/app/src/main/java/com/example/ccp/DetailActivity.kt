baa64aa21eb67c5a281a5420578694cde5856549
HwangJanghyeon <janghyeonh@naver.com>
2024-03-26 11:29:25 +0900

    DetailActivity 레이아웃 추가

--- ccp/app/src/main/java/com/example/ccp/DetailActivity.kt
+++ ccp/app/src/main/java/com/example/ccp/DetailActivity.kt
@@ -2,7 +2,6 @@ package com.example.ccp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.ccp.R
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
