80f93713f986fffd4900ee0089e87b846f5fd3e6
HwangJanghyeon <janghyeonh@naver.com>
2024-04-02 12:22:38 +0900

    댓글 삭제 기능 추가

--- ccp_(0329_오전1032취합1)/ccp/app/src/main/java/com/example/ccp/adapter/CommentAdapter.kt
+++ ccp_(0329_오전1032취합1)/ccp/app/src/main/java/com/example/ccp/adapter/CommentAdapter.kt
@@ -6,9 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ccp.R
 import com.example.ccp.model.CommentDTO
+import com.example.ccp.util.RetrofitClient.apiService
+import com.example.ccp.util.RetrofitClient.commentService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class CommentAdapter(
     private val context: Context,
@@ -42,6 +48,7 @@ class CommentAdapter(
             // 댓글 삭제 버튼 클릭 이벤트 처리
             linkDeleteComment.setOnClickListener{
                 Log.d("deleteComment","댓글 삭제 버튼")
+                deleteCommentToServer(comment.cnum)
             }
         }
     }
@@ -50,5 +57,28 @@ class CommentAdapter(
     fun updateData(newCommentList: List<CommentDTO>) {
         commentList = newCommentList
         notifyDataSetChanged()
+    }
+
+    private fun deleteCommentToServer(cnum : Long?){
+        cnum?.let { cnumNotNull ->
+            commentService.deleteComments(cnumNotNull.toInt()).enqueue(object : Callback<Void> {
+                override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                    if (response.isSuccessful) {
+                        Toast.makeText(context, "댓글이 삭제되었습니다.", Toast.LENGTH_SHORT).show()
+                        // 서버에서 성공적으로 삭제됐을 때 로컬 데이터 갱신
+                        val newList = commentList.toMutableList()
+                        newList.removeIf { it.cnum == cnumNotNull }
+                        updateData(newList)
+                    } else {
+                        Toast.makeText(context, "댓글 삭제에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onFailure(call: Call<Void>, t: Throwable) {
+                    Log.e("CommentAdapter", "댓글 삭제 실패", t)
+                    Toast.makeText(context, "네트워크 오류로 댓글 삭제에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
     }
 }
