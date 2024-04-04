590599d9a95221c7c9a986bf3e045a332fc7d655
HwangJanghyeon <janghyeonh@naver.com>
2024-04-04 11:28:58 +0900

    안드로이드에서 웹뷰로 게시글을 수정완료하면 다시 안드로이드 메인 액티비티로 이동하는 기능 추가

--- ccp0404/ccp/app/src/main/java/com/example/ccp/UpdateActivity.kt
+++ ccp0404/ccp/app/src/main/java/com/example/ccp/UpdateActivity.kt
@@ -1,7 +1,9 @@
 package com.example.ccp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.JavascriptInterface
 import android.webkit.WebViewClient
 import com.example.ccp.databinding.ActivityUpdateBinding
 
@@ -23,6 +25,19 @@ class UpdateActivity : AppCompatActivity() {
         val webView = binding.webviewUpdate
         webView.settings.javaScriptEnabled = true // JavaScript 활성화
         webView.webViewClient = WebViewClient()
+        // JavaScript 인터페이스 설정
+        webView.addJavascriptInterface(WebAppInterface(), "Android")
         webView.loadUrl("http://10.100.103.42:8005/update_for_mobile/$num") // 해당 URL 로드
+    }
+
+    inner class WebAppInterface{
+        // JavaScript에서 호출할 함수 정의
+        @JavascriptInterface
+        fun redirectToMainActivity() {
+            // MainActivity로 이동
+            val intent = Intent(this@UpdateActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish() // 현재 액티비티 종료
+        }
     }
 }
